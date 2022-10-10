8fb07bb9a852adefbea3ce108eb8b5029991e470
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2022-10-10 17:58:57 +0300

    refractored

--- scripts/Card.js
+++ scripts/Card.js
@@ -12,15 +12,15 @@ export class Card {
     }
     generateCard() {
         this._element = this._getTemplate();
-        this._setEventListeners();
         this._image = this._element.querySelector('.place__image');
+        this._setEventListeners();
         this._image.src = this._imageLink;
         this._image.setAttribute('alt', `Фотография: ${this._title}`);
         this._element.querySelector('.place__title').textContent = this._title;
         return this._element;
     }
-    _handleLikeClick() {
-        this._element.querySelector('.place__like-button').classList.toggle('button_active');
+    _handleLikeClick(likeButton) {
+        likeButton.classList.toggle('button_active');
     }
     _handleDeleteClick() {
         this._element.remove();
@@ -29,13 +29,13 @@ export class Card {
         this._handleOpenPopup(this._title, this._imageLink);
     }
     _setEventListeners() {
-        this._element.querySelector('.place__like-button').addEventListener('click', () => {
-            this._handleLikeClick();
+        this._element.querySelector('.place__like-button').addEventListener('click', (evt) => {
+            this._handleLikeClick(evt.target);
         })
         this._element.querySelector('.place__delete-button').addEventListener('click', () => {
             this._handleDeleteClick();
         })
-        this._element.querySelector('.place__image').addEventListener('click', () => {
+        this._image.addEventListener('click', () => {
             this._handleImageClick();
         })
     }
